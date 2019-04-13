790a47481069995379af68c61c5a9f6c83d4cf51
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-04-13 20:27:29 +0200

    Expect a book id when opening the proxy server

--- server/index.js
+++ server/index.js
@@ -7,6 +7,6 @@ const port = 3000;
 const app = express();
 app.listen(port, () => console.log(`listening on port ${port}`));
 
-app.use(express.static(`${__dirname}/../public`));
+app.use('/:id', express.static(`${__dirname}/../public`));
 app.use(morgan('dev'));
 app.use(cors());
