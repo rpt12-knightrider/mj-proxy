2bf60107f4214e9bf7edb935780d48a488979cdc
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-04-25 06:23:44 +0200

    Setting up the environment

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.listen(port, () => console.log(`listening on port ${port}`));
 
